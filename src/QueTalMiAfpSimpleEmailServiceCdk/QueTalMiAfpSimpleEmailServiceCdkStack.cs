99bc9a3be46996f7f11bfc70c7711f497913e977
Benjamín <benjamin.diaze@outlook.com>
2025-08-18 22:29:44 -0400

    Se añade string parameter para dirección emisor de correos por defecto

--- src/QueTalMiAfpSimpleEmailServiceCdk/QueTalMiAfpSimpleEmailServiceCdkStack.cs
+++ src/QueTalMiAfpSimpleEmailServiceCdk/QueTalMiAfpSimpleEmailServiceCdkStack.cs
@@ -1,7 +1,9 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.Route53;
 using Amazon.CDK.AWS.SES;
+using Amazon.CDK.AWS.SSM;
 using Constructs;
+using Newtonsoft.Json;
 using System;
 
 namespace QueTalMiAfpSimpleEmailServiceCdk
@@ -16,10 +18,23 @@ namespace QueTalMiAfpSimpleEmailServiceCdk
             string mailDomain = System.Environment.GetEnvironmentVariable("MAIL_DOMAIN") ?? throw new ArgumentNullException("MAIL_DOMAIN");
             string mailFromDomain = System.Environment.GetEnvironmentVariable("MAIL_FROM_DOMAIN") ?? throw new ArgumentNullException("MAIL_FROM_DOMAIN");
 
+            string nombreDeDefecto = System.Environment.GetEnvironmentVariable("NOMBRE_DE_DEFECTO") ?? throw new ArgumentNullException("NOMBRE_DE_DEFECTO");
+            string correoDeDefecto = System.Environment.GetEnvironmentVariable("CORREO_DE_DEFECTO") ?? throw new ArgumentNullException("CORREO_DE_DEFECTO");
+
             EmailIdentity emailIdentity = new(this, $"{appName}EmailIdentity", new EmailIdentityProps {
                 Identity = Identity.Domain(mailDomain),
                 MailFromDomain = mailFromDomain,
                 MailFromBehaviorOnMxFailure = MailFromBehaviorOnMxFailure.USE_DEFAULT_VALUE,
+            });
+
+            _ = new StringParameter(this, $"{appName}StringParameterDireccionDeDefecto", new StringParameterProps {
+                ParameterName = $"/{appName}/SES/DireccionDeDefecto",
+                Description = $"Direccion por defecto a usar como emisor de correos de la aplicacion {appName}",
+                StringValue = JsonConvert.SerializeObject(new { 
+                    Nombre = nombreDeDefecto,
+                    Correo = correoDeDefecto
+                }),
+                Tier = ParameterTier.STANDARD,
             });
         }
     }
